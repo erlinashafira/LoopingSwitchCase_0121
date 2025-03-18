253bd7e0eb168237dddf6696b4e0b2636dc85c80
errerew <erlina31acc@gmail.com>
2025-03-18 19:10:25 +0700

    pembuatan erulangan file1

---
+++ file1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //pencacah naik
+    int i;
+    for (i = 0; i < 5; i++){
+        cout << "Nilai i = "<< i <<" Lari Keliling Lapangan" << endl;
+    }
+    cout << "Nilai i terakhir : "<< i << endl;
+
+    //pencacah Turun
+    for (i = 5; i > 0; i--){
+        cout << "Nilai 1 = "<< i <<" Lari Keliling Lapangan" << endl;
+    }
+    cout << "Nilai i terakhir : "<< i << endl;
+}
